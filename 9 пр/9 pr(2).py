6d210739ed50b978019c8a7a2aee8c4294e052e3
vanurtw <109265461+vanurtw@users.noreply.github.com>
2022-12-01 14:16:45 +0800

    Add files via upload

---
+++ 9 пр/9 pr(2).py
@@ -0,0 +1,21 @@
+n=int(input('Введите кол-во строк: '))
+m=int(input('Введите кол-во столбцов: '))
+a=[]
+for i in range(n):
+    b=[]
+    for j in range((m)):
+        b.append(int(input(f'Введите {j+1} элемет {i+1} строки: ')))
+    a.append(b)
+for i in range(n):
+    for j in range(m):
+        print(a[i][j], end=' ')
+    print()
+k=int(input('Введите строку в которой элементы расположить в порядке возрастания: '))
+for i in range(m-1):
+    if a[k-1][i]>a[k-1][i+1]:
+        a[k-1][i], a[k-1][i+1]=a[k-1][i+1], a[k-1][i]
+for i in range(n):
+    for j in range(m):
+        print(a[i][j], end=' ')
+    print()
+
