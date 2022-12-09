424efc64f5d2696cd81b0c9c8c1061c7c6cc0889
vanurtw <109265461+vanurtw@users.noreply.github.com>
2022-12-09 17:56:08 +0800

    Add files via upload

--- 10 пр/main1.py
+++ 10 пр/main1.py
@@ -1,12 +1,33 @@
-import requests
-import json
-from pprint import pprint
-url = f"https://api.github.com/repos/MicrosoftDocs/azure-docs"
-user_data = requests.get(url).json()
-d={}
-c=['company', 'created_at', 'email', 'id', 'name', 'url']
-for i in user_data:
-    if i in c:
-       d[i]=user_data[i]
-with open('output.json', 'w') as file:
-    json.dump(d, file)
+def c(f):
+    matrix = [line.replace("\n", "").split() for line in f]
+    f.close()
+    for i in matrix:
+        for j in range(len(i)):
+            i[j] = int(i[j])
+    print(f'Итоговая матрица: {matrix}')
+    mx = 0
+    mn = 0
+    m = 4
+    m1 = list()
+    for i in matrix:
+        for j in range(len(i) - 1):
+            if i[j] > i[j + 1]:
+                mx += 1
+            else:
+                mn += 1
+            print(i[j])
+        if mx == m - 1 or mn == m - 1:
+            m1.append(i)
+        mx = 0
+        mn = 0
+    m2 = []
+    print(m1)
+    for i in m1:
+        for j in range(m):
+            m2.append(int(i[j]))
+    d = max(m2)
+    print(m2)
+    f1 = open('D:\\python(задания)\\10 пр\\Стешковой И.С._У-222_вывод.txt', 'w')
+    f1.write(f'{d}')
+fc = open('D:\\python(задания)\\10 пр\\Стешковой И.С._У-222_ввод.txt', 'r')
+c(fc)
