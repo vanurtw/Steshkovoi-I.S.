41e557c29ff48db909040c0fd6df70ebf8fb621d
vanurtw <109265461+vanurtw@users.noreply.github.com>
2023-04-21 19:38:42 +0300

    Delete 12 pr directory

--- 12 pr/12 pr (1).py
+++
@@ -1,9 +0,0 @@
-def max(a):
-    max=a
-    b=int(input())
-    if b>a:
-        max=b
-    if b==0:
-        return max
-    max b
-a=int(input())
--- 12 pr/12 пр (2).py
+++
@@ -1,9 +0,0 @@
-def maxx(element, maxNum):
-     return element if element > maxNum else maxNum
-maximum=0
-while True:
-    inp = int(input('vvedi: '))
-    if inp==0:
-        print(maximum)
-        break
-    maximum=maxx(inp, maximum)
