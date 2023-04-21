f04a6c3380c0b3af6940317abff0276398dc6ff6
vanurtw <109265461+vanurtw@users.noreply.github.com>
2023-04-21 19:39:37 +0300

    Delete 3 пр directory

--- 3 пр/4.py
+++
@@ -1,9 +0,0 @@
-# 4 задание
-def ln(A,B,L,N):
-    s=2*L+N*A+(N-1)*A+(N-1)*B*2
-    print(f"длина шнурка:{s}")
-a=int(input("a="))
-b=int(input("b="))
-l=int(input("l="))
-n=int(input("n="))
-ln(a,b,l,n)
--- 3 пр/9.py
+++
@@ -1,9 +0,0 @@
-def ras(n,m,k):
-    if (k%m==0 or k%n==0) and k<m*n:
-        print("Да")
-    else:
-        print("Нет")
-N=int(input("n="))
-M=int(input("m="))
-K=int(input("k="))
-ras(N,M,K)
